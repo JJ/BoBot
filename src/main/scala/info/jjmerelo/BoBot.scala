5173a5fdef3af6148d7ef11976b56247bd6347e9
JJ Merelo <jjmerelo@gmail.com>
2016-10-29 19:31:39 +0200

    Elimina variables innecesarias

--- src/main/scala/info/jjmerelo/BoBot.scala
+++ src/main/scala/info/jjmerelo/BoBot.scala
@@ -5,10 +5,9 @@ import com.typesafe.scalalogging.Logger
 
 object BoBot extends TelegramBot with Polling with Commands {
   def token = sys.env("BOBOT_TOKEN")
-  val this_log = Logger("BoBot")
   val lines = scala.io.Source.fromFile("hitos.json").mkString
   on("/hello") { implicit msg => _ =>
     reply("Conseguí que funcionara")
-		this_log.info("Hello")
+		log.info("Hello")
   }
 }
