7d5d93b6a8a517ecfa6b7845f2231bf4e2dd3716
JJ Merelo <jjmerelo@gmail.com>
2016-12-19 17:08:00 +0100

    Preparándose para despliegue en contenedor

--- src/main/scala/info/jjmerelo/BoBot.scala
+++ src/main/scala/info/jjmerelo/BoBot.scala
@@ -6,7 +6,7 @@ import com.typesafe.scalalogging.Logger
 object BoBot extends TelegramBot with Polling with Commands {
   def token = sys.env("BOBOT_TOKEN")
   val lines = scala.io.Source.fromFile("hitos.json").mkString
-  on("/hello") { implicit msg => _ =>
+  on("/hola") { implicit msg => _ =>
     reply("Conseguí que funcionara")
 		log.info("Hello")
   }
