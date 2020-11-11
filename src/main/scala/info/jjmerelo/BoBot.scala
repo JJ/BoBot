66fdd0589cbb3e63c6b7325323e225f153284640
JJ Merelo <jjmerelo@gmail.com>
2020-11-11 07:56:57 +0100

    Conseguí que compilara... excepto JSON

--- src/main/scala/info/jjmerelo/BoBot.scala
+++ src/main/scala/info/jjmerelo/BoBot.scala
@@ -1,19 +1,31 @@
 package info.jjmerelo.BoBot
 
-import com.bot4s.telegram.{api, methods, models, Implicits}
+import cats.instances.future._
+import cats.syntax.functor._
+
+import com.bot4s.telegram.api.RequestHandler
 import com.bot4s.telegram.api.declarative.Commands
 import com.bot4s.telegram.clients.{FutureSttpClient, ScalajHttpClient}
 import com.bot4s.telegram.future.{Polling, TelegramBot}
+
+import scala.util.Try
+import scala.concurrent.Future
+
 import com.typesafe.scalalogging.Logger
 import scala.util.parsing.json._
 
-object BoBot extends TelegramBot with Polling with Commands {
+object BoBot extends TelegramBot
+    with Polling
+    with Commands[Future] {
+
+  val log = Logger("BoBot")
   def token = sys.env("BOBOT_TOKEN")
   val lines = scala.io.Source.fromFile("hitos.json").mkString
   val hitos = JSON.parseFull( lines )
   val solo_hitos = hitos.getOrElse( hitos )
-  on("/hey") { implicit msg => _ =>
-    reply("Conseguí que funcionara")
+
+  onCommand("hey") { implicit msg =>
     log.info("Hello")
+    reply("Conseguí que funcionara").void
   }
 }
